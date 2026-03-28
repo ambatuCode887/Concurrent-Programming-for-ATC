1ec6f225b1c8979230997fbf86b75185e0e1e407
Charls Pua Vui Hau <charlshau03@gmail.com>
2026-03-28 14:04:30 +0800

    finished ATC

--- src/ccp/ATC.java
+++ src/ccp/ATC.java
@@ -9,5 +9,28 @@ package ccp;
  * @author User
  */
 public class ATC {
+    private Airport airport;
+    
+    public ATC(Airport airport){
+        this.airport = airport;
+    }
+    
+    public synchronized Gate requestLanding(int planeNumber){
+        Gate gate = airport.getAvailableGate();
+        
+        if(gate == null){
+            System.out.println("ATC: Landing Permission Denied for Plane-" + planeNumber + ", Airport Full.");
+            return null;
+        }
+        
+        System.out.println("ATC: Landing permission granted for Plane" + planeNumber + ".");
+        return gate;
+    }
+    
+    public synchronized void requestTakeoff(int planeNumber){
+        System.out.println("ATC: Taking-off is granted for Plane-" + planeNumber + ". Runway is free.");
+        airport.exitAirport();        
+    }
+    
     
 }
