802747817e139edcbeb1b8ed255b1f2a12357b83
Charls Pua Vui Hau <charlshau03@gmail.com>
2026-05-03 07:00:51 +0800

    update

--- src/ccp/Semaphore.java
+++ src/ccp/Semaphore.java
@@ -19,19 +19,13 @@ public class Semaphore {
 
     public synchronized void acquire() throws InterruptedException {
         while(permits <= 0) {
-            System.out.println(Thread.currentThread().getName() + 
-                " waiting for " + name + " permit...");
             wait();
         }
         permits--;
-        System.out.println(Thread.currentThread().getName() + 
-            " acquired " + name + " permit. Remaining: " + permits);
     }
 
     public synchronized void release() {
         permits++;
-        System.out.println(Thread.currentThread().getName() + 
-            " released " + name + " permit. Available: " + permits);
         notifyAll();
     }
 
