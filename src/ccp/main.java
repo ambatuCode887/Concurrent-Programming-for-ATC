c2ca9ff726fd2fd1a9c609d0e95089816f4c4cf0
Charls Pua Vui Hau <charlshau03@gmail.com>
2026-05-05 18:54:05 +0800

    update

--- src/ccp/main.java
+++ src/ccp/main.java
@@ -2,39 +2,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
 package ccp;
 /**
  *
  * @author User
  */
-
 import java.util.Random;
+
 public class main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         ATC atc = new ATC();
         Thread atcThread = new Thread(atc);
         atcThread.setName("ATC-Thread");
         atcThread.start();
-       
+
         Random rand = new Random();
         Thread[] planes = new Thread[6];
         
+        int emergencyPlane = rand.nextInt(6);
+
         for(int i = 0; i < 6; i++) {
-            boolean isEmergency = (i == 4);
+            boolean isEmergency = (i == emergencyPlane);
             planes[i] = new Thread(new Airplane(i + 1, isEmergency, atc, atc.getAirport()));
             planes[i].setName("Plane-" + (i + 1) + "-Thread");
             planes[i].start();
-            
-            Thread.sleep(rand.nextInt(2000));
+
+            try {
+                Thread.sleep(rand.nextInt(2000));
+            } catch(InterruptedException ex) {}
         }
-        
-        // Wait for all planes
+
         for(Thread plane : planes) {
             try {
                 plane.join();
             } catch(InterruptedException ex) {}
         }
-        
+
         atc.allPlanesDone();
     }
 }
